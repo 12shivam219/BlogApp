09ef223ce6d2f5a162d25f76e65742968e922d89
shivam <12shivamtiwari219@gmail.com>
2023-04-03 02:05:32 +0530

    circle

--- src/components/MainBlog/MainBlog.jsx
+++ src/components/MainBlog/MainBlog.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import './MainBlog.css'
 export default function MainBlog() {
   return (
    <>
-   <div>
+   <div className='w-full h-full relative'>
+    <div className='block w-[500px] h-[500px] border-[55px] border-solid border-white rounded-full fixed top-[75%] left-1/2 -translate-x-1/2 -translate-y-1/2 m-auto'>
+        <div className="block w-[118px] h-[118px] my-8 mx-auto mini-circle"></div> 
+    </div>
+    {/* border-r-[25px]  */}
     <div>
         <p className=''>Mastering Your Finances: Expert tips and tricks to achieve your financial goals</p>
         <span className=''>File Name*|2 Minute Read</span>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import MainBlog from '../../components/MainBlog/MainBlog'
+import MainBlog from '../../components/MainBlog/MainBlog'
 
 export default function HomePage() {
   return (
     <>
 
-      <main className='w-full h-full max-width m-auto'>
-        {/* <MainBlog /> */}
+      <main className='w-full h-full max-width m-auto white'>
+        <MainBlog />
       </main>
 
     </>
